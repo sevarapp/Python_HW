100266768f7723a02a055b636ab4f56b442f3170
sevarapp <sevarapulatova081@gmail.com>
2025-06-18 14:52:23 +0500

    Update homework.py

--- lesson-20/homework/homework.py
+++ lesson-20/homework/homework.py
@@ -1,1 +1,15 @@
+| Customer ID | First Name | Last Name  | Total Spent |
+| ----------- | ---------- | ---------- | ----------- |
+| 6           | Helena     | Holý       | \$49.62     |
+| 26          | Richard    | Cunningham | \$47.62     |
+| 57          | Luis       | Rojas      | \$46.62     |
+| 46          | Hugh       | O'Reilly   | \$45.62     |
+| 45          | Ladislav   | Kovács     | \$45.62     |
+
+
+
+  | Preference        | Percentage of Customers |
+| ----------------- | ----------------------- |
+| Individual Tracks | 64.41%                  |
+| Full Albums       | 35.59%                  |
 
